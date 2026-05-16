d616e530da0ba7cef40eacf7796f0974178652e5
yennifer <shiksjayne95@gmail.com>
2026-05-16 09:47:00 +0300

    Update ToyCard.jsx

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onLike, onDelete }) {
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{toy.name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={toy.image}
+        alt={toy.name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <p>{toy.likes} Likes </p>
+      <button 
+        className="like-btn"
+        onClick={() => onLike(toy.id)}
+      >
+        Like {"<3"}
+      </button>
+      <button 
+        className="del-btn"
+        onClick={() => onDelete(toy.id)}
+      >
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
